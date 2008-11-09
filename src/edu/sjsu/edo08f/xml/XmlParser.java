66a846ce1b5659884f00e4bbc4338dc8d637febe
oleksiy.yarmula <oleksiy.yarmula@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-11-09 02:43:12 +0000

    xml parser basic structure

---
+++ src/edu/sjsu/edo08f/xml/XmlParser.java
@@ -0,0 +1,39 @@
+package edu.sjsu.edo08f.xml;
+
+import edu.sjsu.edo08f.domain.Course;
+import edu.sjsu.edo08f.domain.Instructor;
+import edu.sjsu.edo08f.services.CourseService;
+
+/**
+ * Created by: Alex Yarmula
+ * Date: Nov 8, 2008
+ */
+public class XmlParser {
+
+    private ObjectToXmlConverter objectToXmlConverter;
+    private CourseService courseService;
+
+    public XmlParser() {
+        objectToXmlConverter = new ObjectToXmlConverter();
+        courseService = new CourseServiceImpl();
+    }
+
+    public String parse (String textMessage) {
+
+        String serviceName = "";
+        String methodName = "";
+
+        // 1. parse xml
+        // 2. call some service
+        Course returnedCourse = courseService.create(new Course(), new Instructor());
+
+        if (serviceName.equals("courseService")) {
+            if (methodName.equals("create") ) {
+                return objectToXmlConverter.getOutputForCreateCourseService(returnedCourse);
+            }
+        }
+
+        return "";
+    }
+
+}
--- src/edu/sjsu/edo08f/xml/XmlReceiver.java
+++ src/edu/sjsu/edo08f/xml/XmlReceiver.java
@@ -1,18 +1,34 @@
 package edu.sjsu.edo08f.xml;
 
+import edu.sjsu.edo08f.objectWrappers.XmlParsingError;
+
 /**
  * Created by: Alex Yarmula
  * Date: Nov 8, 2008
  */
 public class XmlReceiver {
 
+    private XmlVerifier xmlVerifier;
+    private XmlParser xmlParser;
+    private ObjectToXmlConverter objectToXmlConverter;
+
+    public XmlReceiver() {
+        this.xmlVerifier = new XmlVerifier ();
+        this.xmlParser = new XmlParser ();
+        this.objectToXmlConverter = new ObjectToXmlConverter();
+    }
+
     public String processRequest (String textMessage) {
 
-        1. Verify against xsd schema
-        2. Parse to right objects
+        XmlParsingError possibleErrors = xmlVerifier.verifyMessage(textMessage);
+        if (possibleErrors.isNotEmpty()) return getBadXmlMessage(possibleErrors.getErrorMessage()); 
 
-        return "";
+       return xmlParser.parse(textMessage);
 
+    }
+
+    private String getBadXmlMessage (String reason) {
+        return objectToXmlConverter.getBadXmlMessage(reason);
     }
 
 }
