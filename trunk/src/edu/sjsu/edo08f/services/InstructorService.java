6a672252b626c608d6309366be61272cc7bca370
oleksiy.yarmula <oleksiy.yarmula@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-11-10 02:20:02 +0000

    removed exception comments from services. not the exceptions are all listed in documentation

--- trunk/src/edu/sjsu/edo08f/services/InstructorService.java
+++ trunk/src/edu/sjsu/edo08f/services/InstructorService.java
@@ -11,12 +11,12 @@ import java.util.List;
  */
 public interface InstructorService {
 
-    List<Instructor> getAll(); // exceptions: general exception. may return empty list
-    Instructor getById (Long id); //thows general exception, no such instructor, invalid argument
+    List<Instructor> getAll();
+    Instructor getById (Long id);
 
-    Instructor create (Instructor instructor); // invalid instructor, duplicate instructor, general exception, invalid argument exception
-    Instructor update (Instructor instructor); // exceptions: no such instructor, invalid instructor, duplicate instructor, general exception, invalid argument exception
-    void delete (Instructor instructor); // no such instructor, hasDependencies, general exception, invalid argument exception 
+    Instructor create (Instructor instructor);
+    Instructor update (Instructor instructor);
+    void delete (Instructor instructor);
 
-    List<Course> getAssociatedCourses (Instructor instructor); // general exception, no such instructor exception, invalid argument exception
+    List<Course> getAssociatedCourses (Instructor instructor);
 }
