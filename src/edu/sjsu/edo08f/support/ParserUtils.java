8ad4870a7ac4d67c00dcb2578338c6a17bf74b1b
tantan1214 <tantan1214@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-11-17 08:16:40 +0000

    Tan's task - This is my ParserUtils class.

--- src/edu/sjsu/edo08f/support/ParserUtils.java
+++ src/edu/sjsu/edo08f/support/ParserUtils.java
@@ -2,6 +2,7 @@ package edu.sjsu.edo08f.support;
 
 import java.util.*;
 import java.util.ArrayList;
+import java.util.regex.Pattern;
 
 /**
  * Created by: Oleksiy Yarmula
@@ -26,9 +27,9 @@ public class ParserUtils {
 	//or
 	//[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
 	private static final String PATTERN_ZIP_CODE = "(\\d{5}|\\d{5}-\\d{4})";
-	
-	private static final String PATTERN_OFFICE_HOUR = "???????????????????";
-    
+
+    private static final String PATTERN_OFFICE_HOUR = "(M?T?W?R?F?S?U? (([0-1][0-9])|(2[0-3]))[0-5][0-9]-(([0-1][0-9])|(2[0-3]))[0-5][0-9])|TBA";
+    private static final int FIRST_HOUR = 8;
 	/**
      *Modified by Tan Tan, Nov,09,2008
      * @param officeHours in format [M?][T?][W?][R?][F?][S?][U?] [00-23][00-59]-[00-23][00-59]
@@ -37,7 +38,25 @@ public class ParserUtils {
     public static boolean verifyOfficeHours (String officeHours) 
     {
     	// need to implement this method
-    	return Pattern.matches(PATTERN_OFFICE_HOUR, officeHours);
+        if(!Pattern.matches(PATTERN_OFFICE_HOUR,officeHours))
+        {
+            return false;
+        }
+        int positionTime = officeHours.indexOf(" ");
+
+        if(positionTime != -1)
+        {
+            String hours = officeHours.substring(positionTime+1);
+            int startHour = Integer.parseInt(hours.substring(0, 2));
+			int startMinutes = Integer.parseInt(hours.substring(2, 4));
+			int endHour = Integer.parseInt(hours.substring(5, 7));
+			int endMinutes = Integer.parseInt(hours.substring(7, 9));
+			if(endHour != 0 && endHour < startHour) return false;
+			if(endHour == startHour && endMinutes <= startMinutes) return false;
+			if(startHour < FIRST_HOUR) return false;
+			if(endHour == 0 && endMinutes != 0) return false;
+        }
+    	return true;
     }
 
     /*
@@ -75,37 +94,70 @@ public class ParserUtils {
     public static List<EventInformation> parseOfficeHours (List<String> officeHoursLines) 
     {
     	String myOfficeHoursLine;
-    	ArrayList<EventInformation> myArrayList = new ArrayList<EventInformation>; 
-    	
+    	ArrayList<EventInformation> officeHoursArrayList = new ArrayList<EventInformation>();
+
     	for (int i=0; i<officeHoursLines.size(); i++)
     	{
     		myOfficeHoursLine = officeHoursLines.get(i);
-    		String[] tempArr = myOfficeHoursLine.split(" ");
-    		String stringDayOfWeek = tempArr[0];
-    		String stringStartEnd = tempArr[1];
-    		
-    		tempArr = stringStartEnd.split("-");
-    		String StartTime = tempArr[0];
-    		String EndTime = tempArr[1];
-    		
-    		for (int j=0; j<stringDayOfWeek.length(); j++)
-    		{
-    			EventInformation myEventInformation = new EventInformation(); 
-    			String Day = stringDayOfWeek.charAt(j);
-//////////////////////////////////////////////////////////////////
-//    			myEventInformation.setDayOfWeek();
-    			myEventInformation.setStartTime(StartTime);
-    			myEventInformation.setEndTime(EndTime);
-    			myArrayList.add(myEventInformation);
-    		}
-    	}
-        return new myArrayList;
-    }
+            if (verifyOfficeHours(myOfficeHoursLine))
+            {
+                String[] tempArr = myOfficeHoursLine.split(" ");
+                String stringDayOfWeek = tempArr[0];
+                String stringStartEnd = tempArr[1];
 
+                tempArr = stringStartEnd.split("-");
+                String startTime = tempArr[0];
+                String endTime = tempArr[1];
+
+                for (int j=0; j<stringDayOfWeek.length(); j++)
+                {
+                    EventInformation myEventInformation = new EventInformation();
+                    String Day = String.valueOf(stringDayOfWeek.charAt(j));
+                    if (DayOfWeek.isCorrectDayOfWeek(Day))
+                    {
+                        myEventInformation.setDayOfWeek(DayOfWeek.getDayByShortcutName(Day));
+                        myEventInformation.setStartTime(startTime);
+                        myEventInformation.setEndTime(endTime);
+                        officeHoursArrayList.add(myEventInformation);
+                    }
+                }
+            }
+    	}
+        return officeHoursArrayList;
+    }
+    /*
+    * Modified by Tan Tan, Nov,10,2008
+    * This method does the exact opposite things of method "parseOfficeHours"
+     */
     public static List<String> convertOfficeHoursToLines(List<EventInformation> officeHours)
     {
-    	return new ArrayList<EventInformation>();
-    }
+        List<String> officeHoursLines = new ArrayList<String>();
+        while(officeHours.size()>0){
+            EventInformation officeHour  = officeHours.get(0);
+            if (officeHour != null )
+            {
+                officeHours.remove(0);
+                String startTime = officeHour.getStartTime();
+                String endTime   = officeHour.getEndTime();
+                String Days      = officeHour.getDayOfWeek().toString();
+                for( int i=0 ; i <officeHours.size(); i++ ){
+                   EventInformation officeHourTemp  = officeHours.get(i);
+                   String startTimeTemp = officeHourTemp.getStartTime();
+                   String endTimeTemp   = officeHourTemp.getEndTime();
+                   if ( startTime == startTimeTemp && endTime == endTimeTemp){
+                      Days = Days + officeHourTemp.getDayOfWeek().toString();
+                      officeHours.remove(i);
+                      i--;
+                   }
+                }
+                String officeHoursLine = Days + " " + startTime+"-"+endTime;
+                officeHoursLines.add(officeHoursLine);
+            }
+
+        }
+
+    	return officeHoursLines;
+        }
     /*
     The input can be like ENG183, TBATBA
      */
--- test-src/edu/sjsu/edo08f/support/ParserUtilsTest.java
+++ test-src/edu/sjsu/edo08f/support/ParserUtilsTest.java
@@ -6,25 +6,31 @@ import org.testng.annotations.Test;
 
 public class ParserUtilsTest {
 
-    @Test (expectedExceptions = RuntimeException.class);
+    @Test (expectedExceptions = RuntimeException.class)
 
     public void verifyCourseLocationTest ()
     {
-    	ArrayList<String> MyLocationArr = new ArrayList<String>;
-    	MyLocationArr.add("TBA183");
-    	MyLocationArr.add("ENGTBA");
-    	MyLocationArr.add("TBA");
-    	MyLocationArr.add(" ");
+        ArrayList<String> correctLocationArr = new ArrayList<String>();
+    	ArrayList<String> incorrectLocationArr = new ArrayList<String>();
 
-    	for (int i=0; i<MyLocationArr.size(); i++)
+        correctLocationArr.add("TBATBA");
+        correctLocationArr.add("ENG183");
+
+    	incorrectLocationArr.add("TBA183");
+    	incorrectLocationArr.add("ENGTBA");
+    	incorrectLocationArr.add("TBA");
+    	incorrectLocationArr.add(" ");
+
+    	for (int i=0; i<incorrectLocationArr.size(); i++)
     	{
-	    	System.out.print("Verify Course Location: " + Location);
-	    	if (ParserUtils.verifyCourseLocation(MyLocationArr.get(i)))
+	    	System.out.print("Verify Location: " + incorrectLocationArr.get(i));
+	    	if (ParserUtils.verifyCourseLocation(incorrectLocationArr.get(i)))
 	    	{
 	    		System.out.println("  - Pass");
 	    	} else
 	    	{
-	       		System.out.println("  - Error");
+                System.out.println("The pattern [" + incorrectLocationArr.get(i) + "] didn't pass as expected");
+	       		//System.out.println("  - Error");
 	       		throw new RuntimeException();
 	    	}
     	}
@@ -39,22 +45,30 @@ public class ParserUtilsTest {
 	//[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
     public void verifyZipCodeTest()
     {
-    	ArrayList<String> MyZipCodeArr = new ArrayList<String>;
-    	MyZipCodeArr.add("1247");
-    	MyZipCodeArr.add("1829A");
-    	MyZipCodeArr.add("37849-392");
-    	MyZipCodeArr.add("56890-392A");
-    	MyZipCodeArr.add("2374-2384");
-    	MyZipCodeArr.add("2374-2384B");
+        ArrayList<String> correctZipCodeArr = new ArrayList<String>();
+    	ArrayList<String> incorrectZipCodeArr = new ArrayList<String>();
 
-    	for (int i=0; i<MyZipCodeArr.size(); i++)
+        correctZipCodeArr.add("95123");
+        correctZipCodeArr.add("95192");
+        correctZipCodeArr.add("10293");
+        correctZipCodeArr.add("90086-1929");
+
+    	incorrectZipCodeArr.add("1247");
+    	incorrectZipCodeArr.add("1829A");
+    	incorrectZipCodeArr.add("37849-392");
+    	incorrectZipCodeArr.add("56890-392A");
+    	incorrectZipCodeArr.add("2374-2384");
+    	incorrectZipCodeArr.add("2374-2384B");
+
+    	for (int i=0; i<incorrectZipCodeArr.size(); i++)
     	{
-	    	System.out.print("Verify Zip Code: " + MyZipCodeArr.get(i));
-	    	if (ParserUtils.verifyZipCode(MyZipCodeArr.get(i))){
+	    	System.out.print("Verify Zip Code: " + incorrectZipCodeArr.get(i));
+	    	if (ParserUtils.verifyZipCode(incorrectZipCodeArr.get(i))){
 	    		System.out.println("  - Pass");
 	    	} else
 	    	{
-	       		System.out.println("  - Error");
+                 System.out.println("The pattern [" + incorrectZipCodeArr.get(i) + "] didn't pass as expected");
+	       		//System.out.println("  - Error");
 	       		throw new RuntimeException();
 	    	}
     	}
@@ -62,23 +76,29 @@ public class ParserUtilsTest {
 
     public void verifyStudentIdTest ()
     {
-    	ArrayList<String> MyStudentIdArr = new ArrayList<String>;
-    	MyStudentIdArr.add("123456789");
-    	MyStudentIdArr.add("1-234-56789");
-    	MyStudentIdArr.add("12-345-6789");
-    	MyStudentIdArr.add("123-456-789");
-    	MyStudentIdArr.add("123-4567-89");
-    	MyStudentIdArr.add("123-4567-89");
-    	MyStudentIdArr.add("123-45678-9");
+        ArrayList<String> correctStudentIdArr = new ArrayList<String>();
+    	ArrayList<String> incorrectStudentIdArr = new ArrayList<String>();
 
-    	for (int i=0; i<MyStudentIdArr.size(); i++)
+        correctStudentIdArr.add("352-56-4789");
+        correctStudentIdArr.add("006-14-5715");
+
+    	incorrectStudentIdArr.add("123456789");
+    	incorrectStudentIdArr.add("1-234-56789");
+    	incorrectStudentIdArr.add("12-345-6789");
+    	incorrectStudentIdArr.add("123-456-789");
+    	incorrectStudentIdArr.add("123-4567-89");
+    	incorrectStudentIdArr.add("123-4567-89");
+    	incorrectStudentIdArr.add("123-45678-9");
+
+    	for (int i=0; i<incorrectStudentIdArr.size(); i++)
     	{
-	    	System.out.print("Verify Student Id: " + MyStudentIdArr.get(i));
-	    	if (ParserUtils.verifyStudentId(MyStudentIdArr.get(i))){
+	    	System.out.print("Verify Student Id: " + incorrectStudentIdArr.get(i));
+	    	if (ParserUtils.verifyStudentId(incorrectStudentIdArr.get(i))){
 	    		System.out.println("  - Pass");
 	    	} else
 	    	{
-	       		System.out.println("  - Error");
+                System.out.println("The pattern [" + incorrectStudentIdArr.get(i) + "] didn't pass as expected");
+	       		//System.out.println("  - Error");
 	       		throw new RuntimeException();
 	    	}
     	}
@@ -86,23 +106,31 @@ public class ParserUtilsTest {
 
     public void verifyOfficeHoursTest()
     {
-    	ArrayList<String> MyOfficeHoursArr = new ArrayList<String>;
-    	MyOfficeHoursArr.add("WM 0800 - 1000");
-    	MyOfficeHoursArr.add("TTT 1000 - 1200");
-    	MyOfficeHoursArr.add("M 1000");
-    	MyOfficeHoursArr.add("1200 - 1400");
-    	MyOfficeHoursArr.add("MTWF 1400 - 1200");
-    	MyOfficeHoursArr.add("MTWRF 1000 - 1200");
-    	MyOfficeHoursArr.add("MWF 2300 - 0230");
+        ArrayList<String> correctOfficeHoursArr = new ArrayList<String>();
+    	ArrayList<String> incorrectOfficeHoursArr = new ArrayList<String>();
 
-    	for (int i=0; i<MyOfficeHoursArr.size(); i++)
+        correctOfficeHoursArr.add("MWF 0800-1000");
+        correctOfficeHoursArr.add("TR 1200-1400");
+
+    	incorrectOfficeHoursArr.add("WM 0800 - 1000");
+        incorrectOfficeHoursArr.add("MWF 1200 - 1400");
+    	incorrectOfficeHoursArr.add("TTT 1000-1200");
+        incorrectOfficeHoursArr.add("T 1000 - 1200");
+    	incorrectOfficeHoursArr.add("M 1000");
+    	incorrectOfficeHoursArr.add("1200-1400");
+    	incorrectOfficeHoursArr.add("MTWF 1400 - 1200");
+    	incorrectOfficeHoursArr.add("MTWRF 1000 - 1200");
+    	incorrectOfficeHoursArr.add("MWF 2300 - 0230");
+
+    	for (int i=0; i<incorrectOfficeHoursArr.size(); i++)
     	{
-	    	System.out.print("Verify Office Hours: " + MyOfficeHoursArr.get(i));
-	    	if (ParserUtils.verifyOfficeHours(MyOfficeHoursArr.get(i))){
+	    	System.out.print("Verify Office Hours: " + incorrectOfficeHoursArr.get(i));
+	    	if (ParserUtils.verifyOfficeHours(incorrectOfficeHoursArr.get(i))){
 	    		System.out.println("  - Pass");
 	    	} else
 	    	{
-	       		System.out.println("  - Error");
+                System.out.println("The pattern [" + incorrectOfficeHoursArr.get(i) + "] didn's pass as expected");
+	       		//System.out.println("  - Error");
 	       		throw new RuntimeException();
 	    	}
     	}
