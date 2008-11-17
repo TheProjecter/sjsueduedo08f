8fa874a9a7cdeb8a3bf912e941cd509a3cb7fd00
oleksiy.yarmula <oleksiy.yarmula@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-11-17 03:46:53 +0000

    many updates for services

--- src/edu/sjsu/edo08f/exceptions/DuplicateCourseException.java
+++ src/edu/sjsu/edo08f/exceptions/DuplicateCourseException.java
@@ -9,4 +9,9 @@ public class DuplicateCourseException extends GeneralException {
     public DuplicateCourseException(String message) {
         super(message);
     }
+
+    public DuplicateCourseException(String name, Integer section) {
+        super (String.format("The course with name %s and section %d already exists",
+                name, section));
+    }
 }
---
+++ src/edu/sjsu/edo08f/exceptions/ScheduleConflictException.java
@@ -0,0 +1,12 @@
+package edu.sjsu.edo08f.exceptions;
+
+/**
+ * Created by: Alex Yarmula
+ * Date: Nov 16, 2008
+ */
+public class ScheduleConflictException extends GeneralException {
+
+    public ScheduleConflictException(String message) {
+        super(message);
+    }
+}
