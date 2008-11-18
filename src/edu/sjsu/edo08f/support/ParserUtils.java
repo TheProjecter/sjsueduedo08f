9c9a2669b83196e4f2119586a223594a84a90f52
oleksiy.yarmula <oleksiy.yarmula@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-11-18 19:44:29 +0000

    improved Parser Utils

--- src/edu/sjsu/edo08f/support/ParserUtils.java
+++ src/edu/sjsu/edo08f/support/ParserUtils.java
@@ -1,62 +1,54 @@
 package edu.sjsu.edo08f.support;
 
-import java.util.*;
-import java.util.ArrayList;
-import java.util.regex.Pattern;
+import edu.sjsu.edo08f.exceptions.InvalidArgumentException;
 
-/**
- * Created by: Oleksiy Yarmula
- * Date: Oct 12, 2008
- */
+import java.util.*;
+import java.util.regex.Pattern;
 
 /**
  * This class will be used for general functions, like parse office hours and validate if
  * they are correct, or assert other string literals.
- * PLEASE NOTE that this class should have only PUBLIC STATIC methods
- * and thus don't have any fields. 
  */
 
 public class ParserUtils {
 
-	//Student Ids are required to match the pattern of [0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]
+    //Student Ids are required to match the pattern of [0-9][0-9][0-9]-[0-9][0-9]-[0-9][0-9][0-9][0-9]
     //For example, 111-22-3333
-	private static final String PATTERN_STUDENT_ID = "\\d{3}-\\d{2}-\\d{4}";
-	
-	//Zip code is required to match these two patterns
-	//[0-9][0-9][0-9][0-9][0-9]
-	//or
-	//[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
-	private static final String PATTERN_ZIP_CODE = "(\\d{5}|\\d{5}-\\d{4})";
+    private static final String PATTERN_STUDENT_ID = "\\d{3}-\\d{2}-\\d{4}";
+
+    //Zip code is required to match these two patterns
+    //[0-9][0-9][0-9][0-9][0-9]
+    //or
+    //[0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
+    private static final String PATTERN_ZIP_CODE = "(\\d{5}|\\d{5}-\\d{4})";
 
     private static final String PATTERN_OFFICE_HOUR = "(M?T?W?R?F?S?U? (([0-1][0-9])|(2[0-3]))[0-5][0-9]-(([0-1][0-9])|(2[0-3]))[0-5][0-9])|TBA";
     private static final int FIRST_HOUR = 8;
-	/**
+    /**
      *Modified by Tan Tan, Nov,09,2008
      * @param officeHours in format [M?][T?][W?][R?][F?][S?][U?] [00-23][00-59]-[00-23][00-59]
      * @return if this is valid
      */
-    public static boolean verifyOfficeHours (String officeHours) 
+    public static boolean verifyOfficeHours (String officeHours)
     {
-    	// need to implement this method
+        // need to implement this method
         if(!Pattern.matches(PATTERN_OFFICE_HOUR,officeHours))
         {
             return false;
         }
         int positionTime = officeHours.indexOf(" ");
 
-        if(positionTime != -1)
-        {
-            String hours = officeHours.substring(positionTime+1);
-            int startHour = Integer.parseInt(hours.substring(0, 2));
-			int startMinutes = Integer.parseInt(hours.substring(2, 4));
-			int endHour = Integer.parseInt(hours.substring(5, 7));
-			int endMinutes = Integer.parseInt(hours.substring(7, 9));
-			if(endHour != 0 && endHour < startHour) return false;
-			if(endHour == startHour && endMinutes <= startMinutes) return false;
-			if(startHour < FIRST_HOUR) return false;
-			if(endHour == 0 && endMinutes != 0) return false;
-        }
-    	return true;
+        String hours = officeHours.substring(positionTime+1);
+        int startHour = Integer.parseInt(hours.substring(0, 2));
+        int startMinutes = Integer.parseInt(hours.substring(2, 4));
+        int endHour = Integer.parseInt(hours.substring(5, 7));
+        int endMinutes = Integer.parseInt(hours.substring(7, 9));
+        if(endHour != 0 && endHour < startHour) return false;
+        if(endHour == startHour && endMinutes <= startMinutes) return false;
+        if(startHour < FIRST_HOUR) return false;
+        if(endHour == 0 && endMinutes != 0) return false;
+
+        return true;
     }
 
     /*
@@ -67,8 +59,8 @@ public class ParserUtils {
     or
     [0-9][0-9][0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]
      */
-    public static boolean verifyZipCode (String zipCode) 
-    {  
+    public static boolean verifyZipCode (String zipCode)
+    {
         //need to implement this method
         return Pattern.matches(PATTERN_ZIP_CODE, zipCode);
     }
@@ -91,14 +83,14 @@ public class ParserUtils {
     Even if input is provided as "MWF 1800-2000", it still has to be 3 records returnes.
      For list of 2 records : "MW 1800-2000" and "S 1000-1200" the method must return list with 3 records
      */
-    public static List<EventInformation> parseOfficeHours (List<String> officeHoursLines) 
+    public static List<EventInformation> parseOfficeHours (List<String> officeHoursLines)
     {
-    	String myOfficeHoursLine;
-    	ArrayList<EventInformation> officeHoursArrayList = new ArrayList<EventInformation>();
+        String myOfficeHoursLine;
+        ArrayList<EventInformation> officeHoursArrayList = new ArrayList<EventInformation>();
 
-    	for (int i=0; i<officeHoursLines.size(); i++)
-    	{
-    		myOfficeHoursLine = officeHoursLines.get(i);
+        for (int i=0; i<officeHoursLines.size(); i++)
+        {
+            myOfficeHoursLine = officeHoursLines.get(i);
             if (verifyOfficeHours(myOfficeHoursLine))
             {
                 String[] tempArr = myOfficeHoursLine.split(" ");
@@ -121,8 +113,10 @@ public class ParserUtils {
                         officeHoursArrayList.add(myEventInformation);
                     }
                 }
+            } else {
+                throw new InvalidArgumentException("The office hours are formatted illegally or have errors in logic");
             }
-    	}
+        }
         return officeHoursArrayList;
     }
     /*
@@ -132,38 +126,64 @@ public class ParserUtils {
     public static List<String> convertOfficeHoursToLines(List<EventInformation> officeHours)
     {
         List<String> officeHoursLines = new ArrayList<String>();
-        while(officeHours.size()>0){
-            EventInformation officeHour  = officeHours.get(0);
+
+        List<EventInformation> officeHoursCopy = copyElementsFromSource (officeHours);
+
+        while(officeHoursCopy.size()>0) {
+            EventInformation officeHour  = officeHoursCopy.get(0);
             if (officeHour != null )
             {
-                officeHours.remove(0);
+                officeHoursCopy.remove(0);
                 String startTime = officeHour.getStartTime();
                 String endTime   = officeHour.getEndTime();
-                String Days      = officeHour.getDayOfWeek().toString();
-                for( int i=0 ; i <officeHours.size(); i++ ){
-                   EventInformation officeHourTemp  = officeHours.get(i);
-                   String startTimeTemp = officeHourTemp.getStartTime();
-                   String endTimeTemp   = officeHourTemp.getEndTime();
-                   if ( startTime == startTimeTemp && endTime == endTimeTemp){
-                      Days = Days + officeHourTemp.getDayOfWeek().toString();
-                      officeHours.remove(i);
-                      i--;
-                   }
+                String days      = officeHour.getDayOfWeek().getShortcutName();
+                for( int i=0 ; i <officeHoursCopy.size(); i++ ){
+                    EventInformation officeHourTemp  = officeHoursCopy.get(i);
+                    String startTimeTemp = officeHourTemp.getStartTime();
+                    String endTimeTemp   = officeHourTemp.getEndTime();
+                    if ( startTime.equals( startTimeTemp ) && endTime.equals (endTimeTemp) ) {
+                        days = days + officeHourTemp.getDayOfWeek().getShortcutName();
+                        officeHoursCopy.remove(i);
+                        i--;
+                    }
                 }
-                String officeHoursLine = Days + " " + startTime+"-"+endTime;
+                String officeHoursLine = days + " " + startTime+"-"+endTime;
                 officeHoursLines.add(officeHoursLine);
             }
 
         }
 
-    	return officeHoursLines;
-        }
+        return officeHoursLines;
+    }
     /*
     The input can be like ENG183, TBATBA
      */
     public static boolean verifyCourseLocation (String courseLocation) {
         return true;
     }
-    
+
+    private static List<EventInformation> copyElementsFromSource (List<EventInformation> source) {
+        List<EventInformation> officeHoursCopy = new ArrayList<EventInformation>();
+        for (EventInformation oneEvent : source) {
+            officeHoursCopy.add(oneEvent);
+        }
+        Collections.sort(officeHoursCopy, new Comparator<EventInformation> () {
+            public int compare (EventInformation one, EventInformation two) {
+                return getValue(one.getDayOfWeek()) - getValue(two.getDayOfWeek());
+            }
+
+            private int getValue (DayOfWeek day) {
+                if (day == DayOfWeek.Monday) return 1;
+                if (day == DayOfWeek.Tuesday) return 2;
+                if (day == DayOfWeek.Wednesday) return 3;
+                if (day == DayOfWeek.Thursday) return 4;
+                if (day == DayOfWeek.Friday) return 5;
+                if (day == DayOfWeek.Saturday) return 6;
+                if (day == DayOfWeek.Sunday) return 7;
+                return 0;
+            }
+        });
+        return officeHoursCopy;
+    }
 
 }
