767f89ea33a99238ebbb01b31095b56cf4cd6b81
sjsu.sandipgd <sjsu.sandipgd@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-12-04 07:14:08 +0000

    commited partial part and still need to do modification.

--- trunk/src/edu/sjsu/edo08f/xml/ObjectToXmlConverter.java
+++ trunk/src/edu/sjsu/edo08f/xml/ObjectToXmlConverter.java
@@ -2,6 +2,12 @@ package edu.sjsu.edo08f.xml;
 
 import edu.sjsu.edo08f.exceptions.GeneralException;
 import edu.sjsu.edo08f.domain.Course;
+import edu.sjsu.edo08f.domain.Student;
+import edu.sjsu.edo08f.domain.Person;
+import edu.sjsu.edo08f.domain.Instructor;
+import com.thoughtworks.xstream.*;
+
+import java.util.List;
 
 /**
  * Created by: Alex Yarmula
@@ -9,6 +15,8 @@ import edu.sjsu.edo08f.domain.Course;
  */
 public class ObjectToXmlConverter {
 
+    XStream xs = new XStream();
+    String xml;
     private String getExceptionXmlTemplate() {
         return "<exception>" +
                         "<name>" +
@@ -27,9 +35,104 @@ public class ObjectToXmlConverter {
     public String getExceptionXmlMessage (Exception exception) {
         return String.format (getExceptionXmlTemplate(), exception.getClass().getName(), exception.getMessage());
     }
+    // Output for StudentServices
 
-    public String getOutputForCreateCourseService (Course course) {
-        return  "<some xml>";
+    public String getOutputForAllStudent(List<Student> student) {
+        xml  = xs.toXML(student);
+        return  xml;
+    }
+    public String getOutputForGetByIdStudentService (Student student) {
+        xml  = xs.toXML(student);
+        return  xml;
+    }
+    public String getOutputForCreateStudentService (Student student) {
+        xml  = xs.toXML(student);
+        return  xml;
+    }
+    public String getOutputForUpdateStudentService (Student student) {
+        xml  = xs.toXML(student);
+        return  xml;
     }
 
+    public String getOutputForDeleteStudentService (Student student) {
+        xml  = xs.toXML(student);
+        return  xml;
+    }
+    public String getOutputForGenerateInvoiceStudentService (String generatInvoice) {
+        xml  = xs.toXML(generatInvoice);
+        return  xml;
+    }
+    public String getOutputForgetAssociatedCoursesStudentService (List<Course> course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+
+    // Output for PersonServices
+
+    public String getOutputForAllPerson(List<Person> person) {
+        xml  = xs.toXML(person);
+        return  xml;
+    }
+    public String getOutputForGetByIdPersonService (Person person) {
+        xml  = xs.toXML(person);
+        return  xml;
+    }
+    public String getOutputForSearchPerson (List<Person> person) {
+        xml  = xs.toXML(person);
+        return  xml;
+    }
+    
+    // Output for InstructorServices
+
+    public String getOutputForAllInstructor(List<Instructor> instructor) {
+        xml  = xs.toXML(instructor);
+        return  xml;
+    }
+    public String getOutputForGetByIdInstructorService (Instructor instructor) {
+        xml  = xs.toXML(instructor);
+        return  xml;
+    }
+    public String getOutputForCreateInstructorService (Instructor instructor) {
+        xml  = xs.toXML(instructor);
+        return  xml;
+    }
+    public String getOutputForUpdateInstructorService (Instructor instructor) {
+        xml  = xs.toXML(instructor);
+        return  xml;
+    }
+    public String getOutputForgetAssociatedCoursesInstructorService (List<Course> course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+
+    // Output for CourseServices
+
+    public String getOutputForAllCourse(List<Course> course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+    public String getOutputForGetByIdCourseService(Course course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+    public String getOutputForGetStudentsByCourseCourseService(List<Student> student) {
+        xml  = xs.toXML(student);
+        return  xml;
+    }
+    public String getOutputForGetInstructorByCourseCourseService(Instructor instructor) {
+        xml  = xs.toXML(instructor);
+        return  xml;
+    }
+    public String getOutputForCreateCourseService(Course course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+    public String getOutputForUpdateCourseService(Course course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
+    public String getOutputForSearchInstructorCourse(List<Course> course) {
+        xml  = xs.toXML(course);
+        return  xml;
+    }
 }
--- trunk/src/edu/sjsu/edo08f/xml/XmlVerifier.java
+++ trunk/src/edu/sjsu/edo08f/xml/XmlVerifier.java
@@ -2,7 +2,16 @@ package edu.sjsu.edo08f.xml;
 
 import edu.sjsu.edo08f.objectWrappers.XmlParsingError;
 
-import java.io.File;
+import java.io.*;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+import javax.xml.validation.SchemaFactory;
+import javax.xml.transform.Source;
+import javax.xml.transform.stream.StreamSource;
+import javax.jms.TextMessage;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
 
 /**
  * Created by: Alex Yarmula
@@ -14,7 +23,24 @@ public class XmlVerifier {
 
         XmlParsingError possibleErrors = new XmlParsingError();
 
-        File xsdSchema = new File ("address-to-xsd");
+        try {
+            SAXParserFactory factory = SAXParserFactory.newInstance();
+            factory.setValidating(false);
+            factory.setNamespaceAware(true);
+            SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
+            factory.setSchema(schemaFactory.newSchema(new Source[] { new StreamSource("file:///C:/Users/Sandip/Desktop/XML/inputSchema.xsd") }));
+            SAXParser parser = factory.newSAXParser();
+            XMLReader reader = parser.getXMLReader();
+            reader.setErrorHandler(new SimpleErrorHandler());
+            reader.parse(testMessage);
+            //reader.parse(new InputSource("file:///C:/Users/Sandip/Desktop/XML/person.xml"));
+            System.out.println("XML is valid");
+            }
+
+            catch (Exception e) {
+                // TODO: handle exception
+                System.out.println(e);
+            }
         // verification happens here
         //fill possible errors file
         
