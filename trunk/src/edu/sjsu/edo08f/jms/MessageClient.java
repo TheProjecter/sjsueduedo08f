4f4037518d46e4757fa408048c8745844ac771b5
oleksiy.yarmula <oleksiy.yarmula@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-12-07 03:17:22 +0000

    added message client with sample message

---
+++ trunk/src/edu/sjsu/edo08f/jms/MessageClient.java
@@ -0,0 +1,78 @@
+package edu.sjsu.edo08f.jms;
+
+import javax.jms.Topic;
+import javax.jms.JMSException;
+import javax.jms.MessageProducer;
+import javax.jms.TextMessage;
+
+/**
+ * Created by: Alex Yarmula
+ * Date: Dec 6, 2008
+ */
+public class MessageClient extends AbstractJMSPort {
+
+    private Topic replyTopic;
+
+    public static void main(String args[]) {
+        new MessageClient();
+    }
+
+     private void sendAsynchronousMessage (String messageText) throws JMSException {
+        MessageProducer messageProducer = session.createProducer(counterTopic);
+
+        TextMessage textMessage = session.createTextMessage(messageText);
+
+        if (replyTopic == null ) {
+            replyTopic = session.createTemporaryTopic();
+            consumer = session.createConsumer( replyTopic );
+        }
+
+        textMessage.setJMSReplyTo(replyTopic);
+        messageProducer.send(textMessage);
+
+        messageProducer.close();
+    }
+
+    public String getReply () throws JMSException {
+        TextMessage reply = (TextMessage)consumer.receive();
+        return reply.getText();
+    }
+
+    public MessageClient() {
+
+        prepareResources ();
+
+        makeCallSafely();
+    }
+
+
+    private void prepareResources () {
+        activateJndi();
+        createConnection();
+        createSession();
+        lookUpTheTopicByName ("CounterTopic");
+        startConnection();
+    }
+
+    private void makeCallSafely () {
+        try {
+            makeCall ();
+        }   catch(JMSException JMSException) {
+            System.out.println("JMS Exception: "+JMSException);
+        }
+    }
+
+    private void makeCall () throws JMSException {
+
+        sendAsynchronousMessage(
+                "<request>" +
+                "        <studentService>" +
+                "            <getAll/>" +
+                "        </studentService>" +
+                "    </request>"
+        );
+        String reply = getReply();
+        System.out.println(reply);
+    }
+
+}
