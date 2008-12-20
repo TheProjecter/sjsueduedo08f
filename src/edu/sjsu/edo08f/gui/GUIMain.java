97fa6998672ada5a1b58648c0ff075d325bf1708
anita.masand <anita.masand@514bd5a6-9996-11dd-a29e-e3fa11317253>
2008-12-20 22:52:59 +0000

    Added to Main GUIMain.form

--- src/edu/sjsu/edo08f/gui/GUIMain.java
+++ src/edu/sjsu/edo08f/gui/GUIMain.java
@@ -16,7 +16,7 @@ package edu.sjsu.edo08f.gui;
  * @author francist
  */
 public class GUIMain extends javax.swing.JFrame {
-
+public static boolean personFlag = false;
     /** Creates new form GUIMain */
     public GUIMain() {
         initComponents();
@@ -43,20 +43,20 @@ public class GUIMain extends javax.swing.JFrame {
         jLayeredPane1.setBorder(javax.swing.BorderFactory.createTitledBorder(""));
         jLayeredPane1.setName("jLayeredPane1"); // NOI18N
 
-        org.jdesktop.application.ResourceMap resourceMap = org.jdesktop.application.Application.getInstance(MainApp.class).getContext().getResourceMap(GUIMain.class);
-        jButton_Student.setFont(resourceMap.getFont("jButton_Student.font")); // NOI18N
-        jButton_Student.setText(resourceMap.getString("jButton_Student.text")); // NOI18N
         jButton_Student.setName("jButton_Student"); // NOI18N
         jButton_Student.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jButton_StudentMouseClicked(evt);
             }
         });
+        jButton_Student.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_StudentActionPerformed(evt);
+            }
+        });
         jButton_Student.setBounds(90, 200, 280, 30);
         jLayeredPane1.add(jButton_Student, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        jButton_Person.setFont(resourceMap.getFont("jButton_Person.font")); // NOI18N
-        jButton_Person.setText(resourceMap.getString("jButton_Person.text")); // NOI18N
         jButton_Person.setName("jButton_Person"); // NOI18N
         jButton_Person.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -66,8 +66,6 @@ public class GUIMain extends javax.swing.JFrame {
         jButton_Person.setBounds(90, 50, 280, 30);
         jLayeredPane1.add(jButton_Person, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        jButton_Course.setFont(resourceMap.getFont("jButton_Course.font")); // NOI18N
-        jButton_Course.setText(resourceMap.getString("jButton_Course.text")); // NOI18N
         jButton_Course.setName("jButton_Course"); // NOI18N
         jButton_Course.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
@@ -77,12 +75,15 @@ public class GUIMain extends javax.swing.JFrame {
         jButton_Course.setBounds(90, 100, 280, 30);
         jLayeredPane1.add(jButton_Course, javax.swing.JLayeredPane.DEFAULT_LAYER);
 
-        jButton_Instructor.setFont(resourceMap.getFont("jButton_Instructor.font")); // NOI18N
-        jButton_Instructor.setText(resourceMap.getString("jButton_Instructor.text")); // NOI18N
         jButton_Instructor.setName("jButton_Instructor"); // NOI18N
         jButton_Instructor.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseClicked(java.awt.event.MouseEvent evt) {
                 jButton_InstructorMouseClicked(evt);
+            }
+        });
+        jButton_Instructor.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton_InstructorActionPerformed(evt);
             }
         });
         jButton_Instructor.setBounds(90, 150, 280, 30);
@@ -127,6 +128,23 @@ public class GUIMain extends javax.swing.JFrame {
     private void jButton_StudentMouseClicked(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_jButton_StudentMouseClicked
         // Student management:
     }//GEN-LAST:event_jButton_StudentMouseClicked
+
+    private void jButton_InstructorActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_InstructorActionPerformed
+        // TODO add your handling code here:
+         InstructorGUI instructorgui = new InstructorGUI();
+    this.setVisible(false);
+    personFlag = true;
+    instructorgui.setVisible(true);
+    }//GEN-LAST:event_jButton_InstructorActionPerformed
+
+    private void jButton_StudentActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton_StudentActionPerformed
+
+          StudentGUI studentgui = new StudentGUI();
+    this.setVisible(false);
+    personFlag=false;
+    studentgui.setVisible(true);
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jButton_StudentActionPerformed
 
     /**
     * @param args the command line arguments
